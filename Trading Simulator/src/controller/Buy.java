2b4081e80c06afe611711e9b71fc756a47c36905
Andrew <subtonesrock@gmail.com>
2014-02-22 11:21:56 -0800

    Forgot to add files to commit (whoops)

---
+++ Trading Simulator/src/controller/Buy.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Map;
+
+import javax.swing.AbstractAction;
+
+import model.Model;
+
+
+public class Buy extends AbstractAction implements ActionListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Model m;
+	public Buy(Model model){
+		m = model;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//get the current values of all state variables
+		Map<String, Float> values;
+		values = m.getPrices();
+		
+		//get value of btcSlider
+		int amt = m.getSlider();
+		System.out.println(amt);
+		
+		//get $
+		
+		//check to see if enough $ remains
+		//subtract the dollars
+		//add the bitcoin
+		//update the model
+		
+	}
+
+}
